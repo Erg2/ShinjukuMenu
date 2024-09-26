ddaee9c8831e98f4912b275df2808d7cb0811070
Eben Gay <egay@creativemicro.com>
2024-09-26 14:58:57 -0400

    full screen, add req'd modules to readme

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ Open shinjukustationvt.com and sequentially show each of its menu pages
 Copyright © Shinjuku Station restaurant 2024
 
 This is a Python app intended to be run on a Raspberry Pi
+
+# Installed these to get it running on Win 11 WSL
+sudo apt install python3-pip
+pip install selenium
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get install "fonts-liberation"
+sudo apt-get install libasound2
+sudo apt --fix-broken install
+
--- ShinjukuMenu.py
+++ ShinjukuMenu.py
@@ -9,22 +9,39 @@ import time
 import sys
 import traceback
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.chrome.options import Options
+#from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 
-# list of pages
-pageList = ["https://www.shinjukustationvt.com", ]
+## TODO if we run into memory problems, try clearing the cache
+#driver.execute_cdp_cmd('Storage.clearDataForOrigin', {
+#    "origin": '*',
+#    "storageTypes": 'all',
+#})
+
+## Hack transition - try scrolling right till page off screen, go to next page but scrolled way left and then scroll in?
 
 try:
-    driver = webdriver.Chrome()
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option("useAutomationExtension", False)
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    driver = webdriver.Chrome(options=chrome_options)
+
+    #driver = webdriver.Chrome()
     driver.get("https://www.shinjukustationvt.com")
-    print("title:", driver.title)
+    #print("title:", driver.title)
     pageHeight = driver.execute_script("return document.body.scrollHeight")
-    print("Scroll pageHeight", pageHeight)
+    pageWidth = driver.execute_script("return document.body.offsetWidth;")
+
+    #print("Scroll pageHeight", pageHeight, " pageWidth", pageWidth)
+
+    #driver.find_element(By.XPATH, '/html/body').send_keys(Keys.F11)
+    driver.fullscreen_window()
 
     menuDiv = driver.find_element(By.XPATH, '//h2[contains(text(), "Menu")]')
-    print("Menu location", menuDiv.location)
+    #print("Menu location", menuDiv.location)
 
     topOffset = menuDiv.location["y"] - 50
 
@@ -43,51 +60,64 @@ try:
 
     menuOffset = menuList[0].location["y"]
 
+
     while True:
 
         # click on each menu
         for menu in menuList:
-            print("start menu")
+            #print("start menu")
 
             menu.click()        # select menu
-            print("Scroll to menu")
-            scrollCmd = "window.scrollTo({top:" + str(topOffset) +"})"     # Jump to top of page
+            #print("Scroll to menu")
+            scrollCmd = "window.scrollTo(0, "+ str(topOffset) +")"     # Jump to top of page
             driver.execute_script(scrollCmd)
             time.sleep(preDelay)
 
 
             menuDiv = driver.find_element(By.CLASS_NAME, "menu-wrapper")
-            print("Menu size", menuDiv.size)
+            #print("Menu size", menuDiv.size)
             menuHeight = menuDiv.size["height"]
 
             winSize = driver.get_window_size()
             winHeight = winSize["height"]
-            print("winSize", winSize)
+            #print("winSize", winSize)
+
+            currLine = topOffset
 
             if winHeight < menuHeight + menuOffset:
-                print("winHeight", winHeight, " < (menuHeight", menuHeight, " + menuOffset", menuOffset, ") = ", menuHeight + menuOffset)
+                #print("winHeight", winHeight, " < (menuHeight", menuHeight, " + menuOffset", menuOffset, ") = ", menuHeight + menuOffset)
                 #pageHeight - winSize["height"]
-                print("topOffset", topOffset, " winHeight", winHeight, " menuHeight", menuHeight, " menuOffset", menuOffset)
-                print("Scroll from", topOffset, " to", (menuHeight + menuOffset) - winHeight)
+                #print("topOffset", topOffset, " winHeight", winHeight, " menuHeight", menuHeight, " menuOffset", menuOffset)
+                #print("Scroll from", topOffset, " to", (menuHeight + menuOffset) - winHeight)
                 for ln in range(topOffset, (menuHeight + menuOffset) - winHeight, 1):
                     #scrollCmd = f'window.scrollBy(0,{ln});'
                     #scrollCmd = "window.scrollTo({top:" + str(ln) + ", behavior:'smooth'})"
-                    scrollCmd = "window.scrollTo({top:" + str(ln) + "})"
+                    scrollCmd = "window.scrollTo(0, " + str(ln) + ")"
                     driver.execute_script(scrollCmd)
                     time.sleep(scrollDelay)
-            else:
-                print("winHeight", winHeight, " >= (menuHeight", menuHeight, " + menuOffset", menuOffset, ")")
-            print("Done menu")
+                currLine = (menuHeight + menuOffset) - winHeight
 
-            print(">>>>>>>>>>>>>>>>> PostDelay")
+            #else:
+            #    print("winHeight", winHeight, " >= (menuHeight", menuHeight, " + menuOffset", menuOffset, ")")
+            #print("Done menu")
+
+            #print("PostDelay")
             time.sleep(postDelay)
 
-        print("done")
+            # Sideways doesn't work, dunno why not
+            # print("Scroll siceways 1 to ", pageWidth)
+            # for xx in range(1, pageWidth):
+            #     scrollCmd = "window.scrollTo("+str(xx)+"," + str(currLine) + ")"
+            #     print("scrollCmd", scrollCmd)
+            #     driver.execute_script(scrollCmd)
+            #     time.sleep(.01)
+
+        #print("done")
+
 
 except:
     print("Well, that didn't work")
     ex = sys.exception()
     traceback.print_exception(ex)
-
 
 driver.close()
